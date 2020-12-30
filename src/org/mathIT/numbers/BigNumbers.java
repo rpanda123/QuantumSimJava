42547401624096b1586dd5f706fdc5f52b18f7d4
rpanda123 <rohitpanda2@yahoo.com>
2020-12-30 18:01:41 -0500

    Update BigNumbers.java

--- src/org/mathIT/numbers/BigNumbers.java
+++ src/org/mathIT/numbers/BigNumbers.java
@@ -1,7 +1,7 @@
 /*
  * BigNumbers.java
  *
- * Copyright (C) 2007-2009 Andreas de Vries
+
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
