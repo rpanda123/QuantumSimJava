81c728c480edd5cc4d27291fd0611fdc996e5d0f
rpanda123 <rohitpanda2@yahoo.com>
2020-12-30 17:56:35 -0500

    Update Complex.java

--- src/org/mathIT/numbers/Complex.java
+++ src/org/mathIT/numbers/Complex.java
@@ -1,7 +1,7 @@
 /*
  * Complex.java - Class providing static methods for complex numbers.
  *
- * Copyright (C) 2004-2008 Andreas de Vries
+
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
